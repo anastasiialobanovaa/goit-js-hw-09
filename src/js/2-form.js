7bd2552c5ef83a388675cde1a41eea9ce57b1066
Anastasiia Lobanova <anastasiialobanova@Anastasiias-MacBook-Air.local>
2026-05-29 21:15:05 +0200

    completed

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,47 @@
-console.log('Form');
+// maket
+const STORAGE_KEY = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+
+let formData = {
+  email: '',
+  message: '',
+};
+
+// восстановление данных после перезагрузки
+const savedData = localStorage.getItem(STORAGE_KEY);
+
+if (savedData) {
+  formData = JSON.parse(savedData);
+
+  form.elements.email.value = formData.email || '';
+  form.elements.message.value = formData.message || '';
+}
+// сохраняем данные при вводе
+form.addEventListener('input', event => {
+  const { name, value } = event.target;
+
+  formData[name] = value.trim();
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+// submit формы
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  if (formData.email === '' || formData.message === '') {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log(formData);
+
+  localStorage.removeItem(STORAGE_KEY);
+
+  formData = {
+    email: '',
+    message: '',
+  };
+
+  form.reset();
+});
